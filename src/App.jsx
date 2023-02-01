2dccb18dba287af7395b8dcf39253b9f2f22cd27
Andrea Falcon <83451204+falconandrea@users.noreply.github.com>
2023-02-01 18:42:50 +0100

    chore: init project, prettier and env file

--- src/App.jsx
+++ src/App.jsx
@@ -7,47 +7,48 @@ import {
   Image,
   Input,
   SimpleGrid,
-  Text,
-} from '@chakra-ui/react';
-import { Alchemy, Network, Utils } from 'alchemy-sdk';
-import { useState } from 'react';
+  Text
+} from '@chakra-ui/react'
+import { Alchemy, Network, Utils } from 'alchemy-sdk'
+import { useState } from 'react'
 
-function App() {
-  const [userAddress, setUserAddress] = useState('');
-  const [results, setResults] = useState([]);
-  const [hasQueried, setHasQueried] = useState(false);
-  const [tokenDataObjects, setTokenDataObjects] = useState([]);
+function App () {
+  const [userAddress, setUserAddress] = useState('')
+  const [results, setResults] = useState([])
+  const [hasQueried, setHasQueried] = useState(false)
+  const [tokenDataObjects, setTokenDataObjects] = useState([])
 
-  async function getTokenBalance() {
+  async function getTokenBalance () {
     const config = {
-      apiKey: '<-- COPY-PASTE YOUR ALCHEMY API KEY HERE -->',
-      network: Network.ETH_MAINNET,
-    };
+      apiKey: import.meta.env.VITE_ALCHEMY_KEY || '',
+      network: Network.ETH_MAINNET
+    }
 
-    const alchemy = new Alchemy(config);
-    const data = await alchemy.core.getTokenBalances(userAddress);
+    const alchemy = new Alchemy(config)
+    const data = await alchemy.core.getTokenBalances(userAddress)
+    console.log(data)
 
-    setResults(data);
+    setResults(data)
 
-    const tokenDataPromises = [];
+    const tokenDataPromises = []
 
     for (let i = 0; i < data.tokenBalances.length; i++) {
       const tokenData = alchemy.core.getTokenMetadata(
         data.tokenBalances[i].contractAddress
-      );
-      tokenDataPromises.push(tokenData);
+      )
+      tokenDataPromises.push(tokenData)
     }
 
-    setTokenDataObjects(await Promise.all(tokenDataPromises));
-    setHasQueried(true);
+    setTokenDataObjects(await Promise.all(tokenDataPromises))
+    setHasQueried(true)
   }
   return (
-    <Box w="100vw">
+    <Box w='100vw'>
       <Center>
         <Flex
-          alignItems={'center'}
-          justifyContent="center"
-          flexDirection={'column'}
+          alignItems='center'
+          justifyContent='center'
+          flexDirection='column'
         >
           <Heading mb={0} fontSize={36}>
             ERC-20 Token Indexer
@@ -59,61 +60,63 @@ function App() {
         </Flex>
       </Center>
       <Flex
-        w="100%"
-        flexDirection="column"
-        alignItems="center"
-        justifyContent={'center'}
+        w='100%'
+        flexDirection='column'
+        alignItems='center'
+        justifyContent='center'
       >
         <Heading mt={42}>
           Get all the ERC-20 token balances of this address:
         </Heading>
         <Input
           onChange={(e) => setUserAddress(e.target.value)}
-          color="black"
-          w="600px"
-          textAlign="center"
+          color='black'
+          w='600px'
+          textAlign='center'
           p={4}
-          bgColor="white"
+          bgColor='white'
           fontSize={24}
         />
-        <Button fontSize={20} onClick={getTokenBalance} mt={36} bgColor="blue">
+        <Button fontSize={20} onClick={getTokenBalance} mt={36} bgColor='blue'>
           Check ERC-20 Token Balances
         </Button>
 
         <Heading my={36}>ERC-20 token balances:</Heading>
 
-        {hasQueried ? (
-          <SimpleGrid w={'90vw'} columns={4} spacing={24}>
-            {results.tokenBalances.map((e, i) => {
-              return (
-                <Flex
-                  flexDir={'column'}
-                  color="white"
-                  bg="blue"
-                  w={'20vw'}
-                  key={e.id}
-                >
-                  <Box>
-                    <b>Symbol:</b> ${tokenDataObjects[i].symbol}&nbsp;
-                  </Box>
-                  <Box>
-                    <b>Balance:</b>&nbsp;
-                    {Utils.formatUnits(
-                      e.tokenBalance,
-                      tokenDataObjects[i].decimals
-                    )}
-                  </Box>
-                  <Image src={tokenDataObjects[i].logo} />
-                </Flex>
-              );
-            })}
-          </SimpleGrid>
-        ) : (
-          'Please make a query! This may take a few seconds...'
-        )}
+        {hasQueried
+          ? (
+            <SimpleGrid w='90vw' columns={4} spacing={24}>
+              {results.tokenBalances.map((e, i) => {
+                return (
+                  <Flex
+                    flexDir='column'
+                    color='white'
+                    bg='blue'
+                    w='20vw'
+                    key={i}
+                  >
+                    <Box>
+                      <b>Symbol:</b> ${tokenDataObjects[i].symbol}&nbsp;
+                    </Box>
+                    <Box>
+                      <b>Balance:</b>&nbsp;
+                      {Utils.formatUnits(
+                        e.tokenBalance,
+                        tokenDataObjects[i].decimals
+                      )}
+                    </Box>
+                    <Image src={tokenDataObjects[i].logo} />
+                  </Flex>
+                )
+              })}
+            </SimpleGrid>
+            )
+          : (
+              'Please make a query! This may take a few seconds...'
+            )}
       </Flex>
     </Box>
-  );
+  )
 }
 
-export default App;
+export default App
